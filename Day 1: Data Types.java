411ddfe15a4cef57dbaaecb35c0aec34e819b126
PAWAN KUMAR <33627043+pk27019@users.noreply.github.com>
2020-01-31 23:25:38 +0530

    Create Day 1: Data Types.java

---
+++ Day 1: Data Types.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution 
+{
+	  public static void main(String[] args) 
+    {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+	      Scanner scan = new Scanner(System.in);
+        int x;  double f; String ss;
+        x = scan.nextInt();
+        f = scan.nextDouble();
+        String dd = scan.nextLine();
+        ss = scan.nextLine();
+        String ne = s+ss;
+        System.out.println(x+i);
+        System.out.println(d+f);
+        System.out.println(ne)
+        scan.close();
+    }
+
+}
