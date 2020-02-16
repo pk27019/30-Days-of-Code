43f97bfcf071aca8ff145ac680cba4ba0389f0db
PAWAN KUMAR <33627043+pk27019@users.noreply.github.com>
2020-02-16 23:43:12 +0530

    Create Day 16: Exceptions - String to Integer.java

---
+++ Day 16: Exceptions - String to Integer.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution extends Exception
+ {
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        try
+        { 
+            int x = Integer.parseInt(S); System.out.println(x);
+        }
+        catch(NumberFormatException x){System.out.println("Bad String");}
+    }
+}
+
